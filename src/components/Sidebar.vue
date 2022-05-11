20c9620a18f32fe4afa340e8632275948ef83f96
Lucas <lucas.silva@tsaaviacao.com.br>
2022-05-11 17:27:40 -0300

    configura para branch local

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,39 +4,40 @@
     <div
       :class="isOpen ? 'block' : 'hidden'"
       @click="isOpen = false"
-      class="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"
+      class="
+        fixed
+        inset-0
+        z-20
+        transition-opacity
+        bg-black
+        opacity-50
+        lg:hidden
+      "
     ></div>
     <!-- End Backdrop -->
 
     <div
       :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
-      class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 lg:translate-x-0 lg:static lg:inset-0"
+      class="
+        fixed
+        inset-y-0
+        left-0
+        z-30
+        w-64
+        overflow-y-auto
+        transition
+        duration-300
+        transform
+        bg-gray-900
+        lg:translate-x-0 lg:static lg:inset-0
+      "
     >
-      <div class="flex items-center justify-center mt-8">
+      <div class="flex items-center justify-center mt-1">
         <div class="flex items-center">
-          <svg
-            class="w-12 h-12"
-            viewBox="0 0 512 512"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M364.61 390.213C304.625 450.196 207.37 450.196 147.386 390.213C117.394 360.22 102.398 320.911 102.398 281.6C102.398 242.291 117.394 202.981 147.386 172.989C147.386 230.4 153.6 281.6 230.4 307.2C230.4 256 256 102.4 294.4 76.7999C320 128 334.618 142.997 364.608 172.989C394.601 202.981 409.597 242.291 409.597 281.6C409.597 320.911 394.601 360.22 364.61 390.213Z"
-              fill="#4C51BF"
-              stroke="#4C51BF"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M201.694 387.105C231.686 417.098 280.312 417.098 310.305 387.105C325.301 372.109 332.8 352.456 332.8 332.8C332.8 313.144 325.301 293.491 310.305 278.495C295.309 263.498 288 256 275.2 230.4C256 243.2 243.201 320 243.201 345.6C201.694 345.6 179.2 332.8 179.2 332.8C179.2 352.456 186.698 372.109 201.694 387.105Z"
-              fill="white"
-            />
-          </svg>
-
-          <span class="mx-2 text-2xl font-semibold text-white"
-            >V-Dashboard</span
-          >
+          <img
+            class="w-30 h-30"
+            src="\public\imgs\Followair-Marca-Oficial-_aberto_fechado_monocromatico.png"
+          />
         </div>
       </div>
 
@@ -94,7 +95,7 @@
             />
           </svg>
 
-          <span class="mx-4">UI Elements</span>
+          <span class="mx-4">Elementos UI</span>
         </router-link>
 
         <router-link
@@ -122,7 +123,7 @@
             />
           </svg>
 
-          <span class="mx-4">Tables</span>
+          <span class="mx-4">Tabelas</span>
         </router-link>
 
         <router-link
@@ -149,9 +150,18 @@
           :class="[$route.name === 'Cards' ? activeClass : inactiveClass]"
           to="/cards"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-5 h-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
             <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
-            <path fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd" />
+            <path
+              fill-rule="evenodd"
+              d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
+              clip-rule="evenodd"
+            />
           </svg>
 
           <span class="mx-4">Cards</span>
@@ -162,11 +172,22 @@
           :class="[$route.name === 'Modal' ? activeClass : inactiveClass]"
           to="/modal"
         >
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
-          <path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
-          <path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-5 h-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z"
+            />
+            <path
+              d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z"
+            />
+            <path
+              d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z"
+            />
+          </svg>
 
           <span class="mx-4">Modal</span>
         </router-link>
@@ -182,8 +203,15 @@
             />
           </svg>
 
-          <span class="mx-4">Blank</span>
+          <span class="mx-4">Pagina vazia</span>
         </router-link>
+        <footer class="footer px-4 py-6">
+          <div class="footer-content">
+            <p class="text-sm text-gray-300 text-center">
+              Novo Followair - Desenvolvido por padeiros
+            </p>
+          </div>
+        </footer>
       </nav>
     </div>
   </div>
